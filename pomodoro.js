92f3c033dbeab234186f22fd2d732d83cedd40f6
angelina-tsuboi <angelina.t1832@gmail.com>
2020-08-01 12:44:42 -0700

    pomodoro complete

--- pomodoro.js
+++ pomodoro.js
@@ -32,12 +32,47 @@ class Pomodoro {
     }
 
     setPomodoro(reps){
-        for(let i = 0; i < reps; i++){
-            //do timer stufff
-        }
+        this.startTimer(25, "work", reps);
     }
 
+    startTimer(duration, message, amount) {
+        let timeDuration = duration * 60;
+        var timer = timeDuration, minutes, seconds;
+        var refreshIntervalId = setInterval(function () {
+            minutes = parseInt(timer / 60, 10);
+            seconds = parseInt(timer % 60, 10);
+    
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+    
+            console.log(minutes + ":" + seconds);
+    
+            if (--timer < 0) {
+                console.log(message);
+                console.log('\u0007');
+                clearInterval(refreshIntervalId);
+                if(amount === 0){
+                    console.log("POMODORO COMPLETE!!!");
+                    this.ask();
+                }
+                if(message === "work"){
+                    this.startTimer(5, "break", amount);
+                }
+
+                if(message === "break"){
+                    amount--;
+                    if(amount === 0){
+                        console.log("POMODORO COMPLETE!!!");
+                        return;
+                    }else{
+                        this.startTimer(25, "work", amount);
+                    }
+
+                }
+            }
+        }, 1000);
+    }
 
 }
 
-module.exports = Todo;
+module.exports = Pomodoro;
