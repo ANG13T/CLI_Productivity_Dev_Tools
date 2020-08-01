1fcb570160369e9c119515fab0e189108ee76727
angelina-tsuboi <angelina.t1832@gmail.com>
2020-07-31 21:15:01 -0700

    started quick links

--- quicklinks.js
+++ quicklinks.js
@@ -3,7 +3,7 @@ const clc = require('cli-color');
 
 //HELPER METHODS
 
-var checklistComand = [
+var linksCommand = [
     {
       type: 'input',
       name: 'command',
@@ -11,10 +11,75 @@ var checklistComand = [
     }
   ];
 
+var createCommand = [
+{
+    type: 'input',
+    name: 'command',
+    message: 'Enter Link 📟 :'
+}
+];
+
+var viewCommand = [
+{
+    type: 'input',
+    name: 'command',
+    message: 'Enter Quick Links Command 📟 :'
+}
+];
+
+var deleteCommand = [
+    {
+        type: 'input',
+        name: 'command',
+        message: 'Enter Quick Links Command 📟 :'
+    }
+];
 
 
 class QuickLinks {
-    links = [];
+    links = []
+    constructor(){
+        this.links = [];
+        this.ask();
+    }
+
+    ask(){
+        inquirer.prompt(linksCommand).then(answers => {
+            let result = answers.command;
+            if(result != ''){
+                if(command === "CREATE") {
+                    this.create();
+                    this.ask();
+                }
+
+                if(command === "DELETE") {
+                    this.delete();
+                    this.ask();
+                }
+
+                if(command === "VIEW") {
+                    this.view();
+                    this.ask();
+                }
+
+                if(command === "QUIT") return;
+            }else{
+                console.log("Invalid Command")
+            }
+        });
+    }
+
+    create(){
+
+    }
+
+    delete(){
+
+    }
+
+    view(){
+
+    }
 };
 
   module.exports = QuickLinks;
